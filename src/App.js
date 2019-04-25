aeeaf6144b7ada456832423ca98d91111a40461b
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-04-25 17:54:52 +0300

    prepare page for creating new tutor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppContainer from './components/AppContainer/AppContainer';
 import TutorPage from './components/TutorPage/TutorPage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header/Header';
+import NewTutorPage from './components/NewTutorPage/NewTutorPage';
 import './App.scss';
 
 require('dotenv').config();
@@ -14,6 +15,7 @@ class App extends Component {
       <Router>
         <Header />
         <Route exact path="/" component={AppContainer} />
+        <Route exact path="/new-tutor" component={NewTutorPage} />
         <Route path="/tutor/:id" component={TutorPage} />
       </Router>
     );
